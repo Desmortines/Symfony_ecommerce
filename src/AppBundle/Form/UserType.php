a4e59cd1c8ea08a976e8d1a7f8683ad21064ead7
Zaamiqana <xenitag0@gmail.com>
2018-07-11 16:19:14 +0200

    Modif pour voir si UserRepository est bien modifié

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstName')->add('lastName')->add('email')->add('adress')->add('password')->add('order')->add('payMethod')->add('type')->add('marks')->add('comments')->add('theme')->add('isActive')->add('favorites');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+
+}
