1341607a37ca7a02f1e17b428afc40e9d6dffed1
Acaroux <quentinsom@hotmail.fr>
2018-08-24 16:19:30 +0200

    add advance search remove article controller

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -17,50 +17,6 @@ use Symfony\Component\HttpFoundation\Request;
 class ArticleController extends Controller
 {
     /**
-     * Lists all article entities.
-     *
-     * @Route("/", name="article_index")
-     * @Method("GET")
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $articles = $em->getRepository('AppBundle:Article')->findAll();
-
-        return $this->render('article/index.html.twig', array(
-            'articles' => $articles,
-        ));
-    }
-
-    /**
-     * Creates a new article entity.
-     *
-     * @Route("/new", name="article_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $article = new Article();
-        $form = $this->createForm(ArticleType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $article = $form->getData();
-            $em->persist($article);
-            $em->flush();
-
-            return $this->redirectToRoute('article_show', array('id' => $article->getId()));
-        }
-
-        return $this->render('article/new.html.twig', array(
-            'article' => $article,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
      * Finds and displays a article entity.
      *
      * @Route("/{id}", name="article_show")
@@ -74,65 +30,5 @@ class ArticleController extends Controller
             'article' => $article,
             'delete_form' => $deleteForm->createView(),
         ));
-    }
-
-    /**
-     * Displays a form to edit an existing article entity.
-     *
-     * @Route("/{id}/edit", name="article_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Article $article)
-    {
-        $deleteForm = $this->createDeleteForm($article);
-        $editForm = $this->createForm('AppBundle\Form\ArticleType', $article);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('article_edit', array('id' => $article->getId()));
-        }
-
-        return $this->render('article/edit.html.twig', array(
-            'article' => $article,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a article entity.
-     *
-     * @Route("/{id}", name="article_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Article $article)
-    {
-        $form = $this->createDeleteForm($article);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($article);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('article_index');
-    }
-
-    /**
-     * Creates a form to delete a article entity.
-     *
-     * @param Article $article The article entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Article $article)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('article_delete', array('id' => $article->getId())))
-            ->setMethod('DELETE')
-            ->getForm();
     }
 }
