2cc2c725f0bae7cecc5a95e00fada3f18218f830
Elo <elodie.pullara@epitech.eu>
2018-08-30 10:54:21 +0200

    cart controller ok

---
+++ src/AppBundle/Controller/CartController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+
+/**
+ * Cart controller.
+ *
+ * @Route("cart")
+ */
+class CartController extends Controller
+{
+    /**
+     * @Route("/", name="cart_index")
+     */
+    public function indexAction()
+    {
+        return $this->render('cart/index.html.twig');
+    }
+
+    public function quantityAction()
+    {
+
+    }
+}
